bbefb3d1514009836218d51c8f163ec83df1f7b7
EmeraldNZ <ahyuancy@gmail.com>
2018-12-24 16:09:56 +1300

    Update .js

--- .js
+++ .js
@@ -1,3 +1,5 @@
+/*********    Solution #1    *************************************************************************/
+
 function translatePigLatin(str) {
    let vowel = ['a','e','i','o','u'];
    let arr = [...str];
@@ -24,5 +26,29 @@ function translatePigLatin(str) {
       return arr.join('').concat("ay");
    }
 }
+translatePigLatin("california"); //return "aliforniacay"
+translatePigLatin("glove"); //return "oveglay"
+translatePigLatin("eight"); //return "eightway"
 
-console.log(translatePigLatin("gl"));
+
+/*********    Solution #2    *************************************************************************/
+
+function translatePigLatin2(str) {
+  let pigLatin = '';
+    let regex = /[aeiou]/gi;
+    if(str[0].match(regex) !== null){ // Check if the first character is a vowel
+        pigLatin = str + 'way'; 
+    }else if(str.match(regex) === null){ // Check if the string contains only consonants
+        pigLatin = str + 'ay';
+    }else{
+        let vowelIndice = str.indexOf(str.match(regex)[0]); // Find how many consonants before the first vowel
+        pigLatin = str.substring(vowelIndice) + str.substring(0, vowelIndice) + 'ay'; // Add the consonants that were previously omitted and add the ending
+    }
+    return pigLatin;
+}
+
+
+
+
+// Retrieved from
+// https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/pig-latin/
